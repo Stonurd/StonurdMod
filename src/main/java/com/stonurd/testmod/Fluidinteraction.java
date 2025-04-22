cb715de6ae3bc9fda13a9dfe5427f1d0fb803f22
Stonurd <77725068+Stonurd@users.noreply.github.com>
2025-04-22 13:58:42 +0200

    test
    test

--- src/main/java/com/stonurd/testmod/Fluidinteraction.java
+++ src/main/java/com/stonurd/testmod/Fluidinteraction.java
@@ -4,13 +4,21 @@ import com.github.alexmodguy.alexscaves.server.block.ACBlockRegistry;
 
 
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.common.ForgeMod;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidInteractionRegistry;
 import net.minecraftforge.fluids.FluidInteractionRegistry.InteractionInformation;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 
 
 
 public class Fluidinteraction {
+
+      public static final DeferredRegister<Fluid> FLUIDS =
+      DeferredRegister.create(ForgeRegistries.FLUIDS, testmod.MODID);
+
         
         public static void postInit() {
 
@@ -34,16 +42,22 @@ public class Fluidinteraction {
                         (level, currentPos, relativePos, currentState) -> level.getBlockState(currentPos.below()).is(ACBlockRegistry.PRIMAL_MAGMA.get()) && level.getBlockState(relativePos).is(Blocks.BLUE_ICE),
                         ACBlockRegistry.FLOOD_BASALT.get().defaultBlockState()
         ));
-        FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new InteractionInformation(
-                (level, currentPos, relativePos, currentState) -> level.getBlockState(currentPos.below()).is(Blocks.NETHERRACK) && level.getBlockState(relativePos).is(Blocks.BLUE_ICE),
-                Blocks.NETHERRACK.defaultBlockState()
-));
+        //test with vanilla block
+                FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new InteractionInformation(
+                        (level, currentPos, relativePos, currentState) -> level.getBlockState(currentPos.below()).is(Blocks.NETHERRACK) && level.getBlockState(relativePos).is(Blocks.BLUE_ICE),
+                        Blocks.NETHERRACK.defaultBlockState()
+        ));
 
-FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new InteractionInformation(
-                (level, currentPos, relativePos, currentState) -> level.getBlockState(currentPos.below()).is(Blocks.NETHERITE_BLOCK) && level.getBlockState(relativePos).is(Blocks.BLUE_ICE),
-                ACBlockRegistry.FLOOD_BASALT.get().defaultBlockState()
-));
+                FluidInteractionRegistry.addInteraction(ForgeMod.LAVA_TYPE.get(), new InteractionInformation(
+                        (level, currentPos, relativePos, currentState) -> level.getBlockState(currentPos.below()).is(Blocks.NETHERITE_BLOCK) && level.getBlockState(relativePos).is(Blocks.BLUE_ICE),
+                        ACBlockRegistry.FLOOD_BASALT.get().defaultBlockState()
+        ));
 
 
-                }
 }
+
+public static void register(IEventBus eventBus) {
+        FLUIDS.register(eventBus);
+ }
+}
+
--- src/main/java/com/stonurd/testmod/testmod.java
+++ src/main/java/com/stonurd/testmod/testmod.java
@@ -1,7 +1,7 @@
 package com.stonurd.testmod;
 
+import com.github.alexmodguy.alexscaves.server.event.CommonEvents;
 import com.mojang.logging.LogUtils;
-
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,11 +12,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-
 import org.slf4j.Logger;
-
-
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(testmod.MODID)
 public class testmod
@@ -54,10 +50,11 @@ public class testmod
     public testmod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        
-        
+//        Fluidinteraction.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new CommonEvents());
 
 /*
         // Register the Deferred Register to the mod event bus so blocks get registered
@@ -69,16 +66,11 @@ public class testmod
 */
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
         // Register the item to a creative tab
         //modEventBus.addListener(this::addCreative);
-
         // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us        
         ////context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    
-
     }
-
     private void commonSetup(final FMLCommonSetupEvent event) {
 /*      
         // Some common setup code
@@ -100,7 +92,6 @@ public class testmod
             event.accept(EXAMPLE_BLOCK_ITEM);
 */
     }
-
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
@@ -109,7 +100,6 @@ public class testmod
         LOGGER.info("HELLO from server starting");
 */
     }
-
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
@@ -122,6 +112,4 @@ public class testmod
 */
         }
     }
-
-   
 }
